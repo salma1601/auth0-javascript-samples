f9aadcc6c15531db384fd03f4413210ea387c8b1
salma1601 <salmabougacha@hotmail.com>
2025-04-25 19:53:25 +0200

    Adding extension

--- public/js/app.js
+++ public/js/app.js
@@ -83,7 +83,7 @@ const callApi = async () => {
   try {
     const token = await auth0Client.getTokenSilently();
 
-    const response = await fetch("/api/external", {
+    const response = await fetch("/api/external.js", {
       headers: {
         Authorization: `Bearer ${token}`
       }
